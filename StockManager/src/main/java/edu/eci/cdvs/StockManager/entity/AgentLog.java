3913a6ae16aac45b0011766c226effbfd12fee93
santiagoartgut9 <69946591+santiagoartgut9@users.noreply.github.com>
2025-02-27 15:52:53 -0500

    Update AgentLog.java

--- StockManager/src/main/java/edu/eci/cdvs/StockManager/entity/AgentLog.java
+++ StockManager/src/main/java/edu/eci/cdvs/StockManager/entity/AgentLog.java
@@ -7,19 +7,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AgentLog implements Agent{
-    private List<String> logs = new ArrayList<>();
-
-    public AgentLog(){
-    }
+   
 
     public void update(Product producto){
         String mensaje = producto.getName()+": xbox one s -> " + producto.getUnits()+" unidades disponibles";
-        logs.add(mensaje);
+      
         System.out.println(mensaje);
     }
 
-    @Override
-    public List<String> getLog(){
-        return logs;
-    }
+   
 }
