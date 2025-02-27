d3076ebe2eed39bd7d5f362f1624d1f31b605bc1
santiagoartgut9 <69946591+santiagoartgut9@users.noreply.github.com>
2025-02-27 15:53:18 -0500

    Update AgentWarning.java

--- StockManager/src/main/java/edu/eci/cdvs/StockManager/entity/AgentWarning.java
+++ StockManager/src/main/java/edu/eci/cdvs/StockManager/entity/AgentWarning.java
@@ -7,20 +7,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AgentWarning implements Agent{
-    private List<String> logs = new ArrayList<>();
-
-    public AgentWarning(){
-    }
+   
     public void update(Product producto) {
         if (producto.getUnits()<5){
             String mensaje = "ALERTA!!! El stock del Producto: "+producto.getName()+" es muy bajo, solo quedan "+ (producto.getUnits()) +" unidades.";
-            logs.add(mensaje);
+    
             System.out.println(mensaje);
         }
     }
 
-    @Override
-    public List<String> getLog(){
-        return logs;
-    }
+   
 }
